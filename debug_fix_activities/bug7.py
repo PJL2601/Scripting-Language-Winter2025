23820abde6c77aaa2bbd686613aad4bc97a95218
PJL2601 <pr.champ.2626@gmail.com>
2025-01-27 16:28:36 -0500

    Update bug7.py

--- debug_fix_activities/bug7.py
+++ debug_fix_activities/bug7.py
@@ -1,5 +1,10 @@
 # Expected Output: Keep asking until the user enters a positive number
-number = -1
-while number < 0:
-    input("Enter a positive number: ")
+while True:
+    try:
+        number = int(input("Enter a positive number: "))
+        if number >= 0:
+            break
+        print("Please enter a positive number!")
+    except ValueError:
+        print("Please enter a valid number!")
 print("Thank you!")
